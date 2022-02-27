50f9c25c0a34459221413de1ea2593e8e6cdd943
Alex <95425708+AlexFike@users.noreply.github.com>
2022-02-27 19:55:54 +0500

    second task

--- Homework_C#_Seminar_1/Program.cs
+++ Homework_C#_Seminar_1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 //Превая задача:
 Console.Write("Введите первое число: ");
 int value1 = Convert.ToInt32(Console.ReadLine());
@@ -14,5 +14,30 @@ else
     {
         Console.WriteLine(value2 + " больше " + value1);
     }
+*/
+// Вторая задача:
 
+Console.Write("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите второе число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int value3 = Convert.ToInt32(Console.ReadLine());
+
+int value_max = 0;
+
+if(value1 > value_max)
+    {
+       value_max = value1;
+    }
+if(value2 > value_max)
+    {
+        value_max = value2;
+    }
+if(value3 > value_max)
+    {
+        value_max = value3;
+    }
+Console.WriteLine("Наибольшее число " + value_max);
