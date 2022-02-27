1fd62716b7430425238e45cf2fce9fe6258e21dc
Alex <95425708+AlexFike@users.noreply.github.com>
2022-02-27 18:13:57 +0500

    first task

---
+++ Homework_C#_Seminar_1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+//Превая задача:
+Console.Write("Введите первое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите первое число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
+
+if(value1 > value2)
+    {
+        Console.WriteLine("Наибольшее число: " + value1);
+    }
+else
+    {
+        Console.WriteLine("Наибольшее число: " + value2);
+    }
+*/
