613eeb7f7e48eff26145fcb5f1113e74c874854b
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-02 20:43:38 +0500

    task two

--- Homework_C#_Seminar_2/Program.cs
+++ Homework_C#_Seminar_2/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // task one
 int ModNum(int num)
 {
@@ -11,3 +11,25 @@ Console.Write("Введите трехзначное число число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Средняя цифра цисла " + ModNum(number));
+*/
+// task two
+void ModNum2(int Num)
+{
+    if(Num / 100 == 0)
+    {
+        Console.WriteLine("У числа нет третьей цифры");
+    }
+    else
+    {
+        while(Num / 100 > 10)
+        {
+            Num = Num / 10;
+        }
+        Console.WriteLine("Третья цифра числа " + Num % 10);
+    }
+}
+// check
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ModNum2(number);
