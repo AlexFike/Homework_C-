ba255425d3a85ba595b457260aec45a2e3212df2
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-02 18:46:14 +0500

    task one

---
+++ Homework_C#_Seminar_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// task one
+int ModNum(int num)
+{
+    int num2 = num % 100;
+    int num3 = num2 / 10;
+    return num3;
+}
+// check
+Console.Write("Введите трехзначное число число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Средняя цифра цисла " + ModNum(number));
