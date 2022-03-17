ed1e12a3cb9050f50b24ec260ffe49f76fd0fd6e
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-17 13:28:13 +0500

    free task v2

--- Homework_C#_free_task/Program.cs
+++ Homework_C#_free_task/Program.cs
@@ -2,7 +2,7 @@
 //Вывести на экран первые два максимальных и первые два минимальных числа из ряда введенных чисел. 
 //А теперь самое интересное: создавать массивы в процессе решения запрещено
 
-Console.WriteLine("Вводите числа которые будете сравнивать, для завршения введите 0!");
+/*Console.WriteLine("Вводите числа которые будете сравнивать, для завршения введите 0!");
 Console.Write("Введите число: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 MaxMin(Number);
@@ -106,3 +106,83 @@ void MaxMin(int Num)
         }
     } 
 }
+*/
+void Numbers()
+{
+    int Num2 = 1;
+    int Num3 = 1;
+    Console.Write("Введите число: ");
+    int Num = Convert.ToInt32(Console.ReadLine());
+    if(Num == 0) Console.WriteLine("Не введено не одного числа для завершения!");
+    else
+    {
+        Console.Write("Введите следующее число: ");
+        Num2 = Convert.ToInt32(Console.ReadLine());
+        if(Num2 == 0) Console.WriteLine("Введите не менее двух чисел!");
+        else 
+        {
+            Console.Write("Введите следующее число: ");
+            Num3 = Convert.ToInt32(Console.ReadLine());
+            MaxMin(Num, Num2, Num3);
+        }
+    }
+}
+void MaxMin(int Numb, int Numb2, int Numb3)
+{
+    int Max1 = 0;
+    int Max2 = 0;
+    int Min1 = 0;
+    int Min2 = 0;
+    if(Numb3 == 0)
+    {
+        if(Numb >= Numb2)
+            {
+                Max1 = Numb;
+                Max2 = Numb2;
+                Min1 = Numb2;
+                Min2 = Numb;
+                if(Numb3 == 0) Console.WriteLine("Два максимальных числа: " + Max1 + " и " + Max2 + ", а два минимальных: " + Min1 + ", и " + Min2);
+                else MaxMin2(Max1, Max2, Min1, Min2, Numb3);
+            }
+            else
+            {
+                Max1 = Numb2;
+                Max2 = Numb;
+                Min1 = Numb;
+                Min2 = Numb2;
+                if(Numb3 == 0) Console.WriteLine("Два максимальных числа: " + Max1 + " и " + Max2 + ", а два минимальных: " + Min1 + ", и " + Min2);  
+                else MaxMin2(Max1, Max2, Min1, Min2, Numb3);
+            }
+    }
+}
+void MaxMin2(int max1, int max2, int min1, int min2, int number)
+{
+while(number < 0 || number > 0)
+    {
+        if(number > max2)
+        {
+            if(number > max1)
+            {
+                max2 = max1;
+                max1 = number;
+            }   
+            else max2 = number;
+        }
+        if(number < min2)
+        {
+                if(number < min1)
+                {
+                    min2 = min1; 
+                    min1 = number;
+                }
+                else min2 = number;
+        }
+        if(number == 0) break;
+        Console.Write("Введите следующее число: ");
+        number = Convert.ToInt32(Console.ReadLine());        
+    }
+    Console.WriteLine("Два максимальных числа: " + max1 + " и " + max2 + ", а два минимальных: " + min1 + ", и " + min2);
+}
+
+Console.WriteLine("Вводите числа которые будете сравнивать, для завршения введите 0!");
+Numbers();
