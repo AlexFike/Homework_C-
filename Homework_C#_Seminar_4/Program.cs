5ab64704dc924112296cdc29b840f896223e2a99
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-22 21:31:07 +0500

    task two seminar 4

--- Homework_C#_Seminar_4/Program.cs
+++ Homework_C#_Seminar_4/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-
+/*
 int Degree(int A, int B)
 {
     int result = A;
@@ -20,3 +20,22 @@ if(numberB <= 0)
     }
 }
 Console.Write($"{numberB} степень числа {numberA} ровна {Degree(B: numberB, A: numberA)}");
+*/
+//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+ int Sum(int number)
+ {
+    int result = 0;
+    while(Math.Abs(number) % 10 > 0) 
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+ }
+ 
+ Console.Write("Введите целое число: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+ if(N / 10 == 0) Console.WriteLine($"Число {N} состоит из одной цифры!");
+ else Console.WriteLine($"Сумма цифр числа {N} равна {Sum(N)}");
+ 
