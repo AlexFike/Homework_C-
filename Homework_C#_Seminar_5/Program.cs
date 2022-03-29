d0f725900b4ba642a54cbbab5d2df7bd4d25e071
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-29 15:06:36 +0500

    homework seminar 5 task 2

--- Homework_C#_Seminar_5/Program.cs
+++ Homework_C#_Seminar_5/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
 //Напишите программу, которая покажет количество чётных чисел в массиве.
-
+/*
 int ThreeNumber(int size)
 {
     int[] ar = new int[size];
@@ -15,3 +15,21 @@ int ThreeNumber(int size)
 
 int SizeArray = 10;
 Console.WriteLine($"Number of even elements in array {ThreeNumber(SizeArray)}");
+*/
+//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int SumEvenNumbers(int size, int start, int end)
+{
+    int[] arR = new int[size];
+    int sum = 0;
+    for(int i = 0; i < size; i++)
+    {
+        arR[i] = new Random().Next(start, end);
+        if(i % 2 != 0) sum = sum + arR[i];
+    }
+    return sum;
+}
+int ArRSize = 10;
+int s = -5;
+int e = 25;
+Console.WriteLine($"Sum of all even numbers in array {SumEvenNumbers(ArRSize, s, e)}");
