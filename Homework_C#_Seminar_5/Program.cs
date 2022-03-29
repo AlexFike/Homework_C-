ddbe6ad58ebd91aa38e942c94a1f1416e5cf800d
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-29 13:58:28 +0500

    homework seminar 5 task 1

---
+++ Homework_C#_Seminar_5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int ThreeNumber(int size)
+{
+    int[] ar = new int[size];
+    int count = 0;
+    for(int i = 0; i < size; i++)
+    {
+        ar[i] = new Random().Next(100, 1000);
+        if(ar[i] % 2 == 0) count++;    
+    }
+    return count;  
+}
+
+int SizeArray = 10;
+Console.WriteLine($"Number of even elements in array {ThreeNumber(SizeArray)}");
