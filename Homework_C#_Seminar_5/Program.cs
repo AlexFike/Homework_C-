61aebe4309a6afb7c40d4df532466183dbd0f6f7
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-29 17:12:08 +0500

    homework seminar 5 task 3

--- Homework_C#_Seminar_5/Program.cs
+++ Homework_C#_Seminar_5/Program.cs
@@ -17,7 +17,7 @@ int SizeArray = 10;
 Console.WriteLine($"Number of even elements in array {ThreeNumber(SizeArray)}");
 */
 //Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-
+/*
 int SumEvenNumbers(int size, int start, int end)
 {
     int[] arR = new int[size];
@@ -33,3 +33,25 @@ int ArRSize = 10;
 int s = -5;
 int e = 25;
 Console.WriteLine($"Sum of all even numbers in array {SumEvenNumbers(ArRSize, s, e)}");
+*/
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+
+double DiffNumbers(int size, int start, int end)
+{
+    double[] ArrD = new double[size];
+    int index = 0;
+    for(int i = 0; i < size; i++) ArrD[i] = new Random().Next(start, end);
+    double min = ArrD[index];
+    double max = ArrD[index];
+    for(int i = 0; i < size; i++)
+    {
+        if(max < ArrD[i]) max = ArrD[i];
+        if(min > ArrD[i]) min = ArrD[i];
+    }
+    double result = max - min;
+    return result;
+}
+int DiffSize = 10;
+int s = -45;
+int e = 25;
+Console.WriteLine($"Difference between the minimum and maximum elements is {DiffNumbers(DiffSize, s, e)}");
