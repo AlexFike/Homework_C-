f8e55ce02cff143177aa93d0710df7707086fba3
Alex <95425708+AlexFike@users.noreply.github.com>
2022-03-31 19:52:21 +0500

    seminar 6 task 1

---
+++ Homework_C#_Seminar_6/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+void QuNum(int num)
+{
+    int count = 0;
+    int i = 0;
+    while(i < num)
+    {
+        Console.Write("Enter a number: ");
+        int ComNum = Convert.ToInt32(Console.ReadLine());
+        if(ComNum > 0) count++;
+        i++;
+    }
+    Console.WriteLine($"Number of numbers greater than zero: {count}");
+}
+
+Console.Write("Enter the number of numbers you will be entering: ");
+int number = Convert.ToInt32(Console.ReadLine());
+QuNum(number);
