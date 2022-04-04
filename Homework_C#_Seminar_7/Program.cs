349ba9e256fc9d1ebfe35f265b4d3790ddf716c8
Alex_Fike <alexfike2016@gmail.com>
2022-04-04 16:49:46 +0500

    homework seminar 7

---
+++ Homework_C#_Seminar_7/Program.cs
@@ -0,0 +1,100 @@
+﻿// // Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// // m = 3, n = 4.
+// // 0,5 7 -2 -0,2
+// // 1 -3,3 8 -9,9
+// // 8 7,8 -7,1 9
+
+// double[,] ArrayRealNumbers(int columns, int rows, int minValueGenerated, int maxValueGenerated, int point)
+// {
+//     double[,] newArray = new double[columns, rows];
+//     Random numbers = new Random();
+//     for (int i = 0; i < newArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < newArray.GetLength(1); j++)
+//         {
+//             newArray[i, j] = Math.Round
+//              ((numbers.Next(minValueGenerated, maxValueGenerated)
+//              + numbers.NextSingle()), point);
+//             //Console.Write("{0,6}", array[i, j]);
+//         }
+//         //Console.WriteLine();
+//     }
+//    return newArray;
+// }
+int enterColumns = 3;
+int enterRows = 4;
+int enterMinValueGenerated = 1;
+int enterMaxValueGenerated = 10;
+int enterNumbersAfterDecimalPoint = 1;
+
+// // Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // 1 7 -> такого числа в массиве нет
+
+// void ElementPositionInArray(int positionColumn, int positionRow, double[,] incomingArray)
+// {
+//     double value = 0;
+//     int tumbler = 0;
+//     for (int i = 0; i < incomingArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < incomingArray.GetLength(1); j++)
+//         {
+//             Console.Write("{0,6} ", incomingArray[i, j]);
+//             if (i + 1 == positionColumn && j + 1 == positionRow)
+//             {
+//             value = incomingArray[i, j];
+//             tumbler = 1;
+//             }
+//         }
+//         Console.WriteLine();
+//     }
+//     if (tumbler == 0) Console.WriteLine("There is no such position in the array");
+//     else Console.WriteLine($"Value of this element = {value}");
+// }
+// Console.Write("Enter a position in the element column: ");
+// int posColumns = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter a position in the item row: ");
+// int posRows = Convert.ToInt32(Console.ReadLine());
+// ElementPositionInArray(posColumns, posRows,
+// ArrayRealNumbers(enterColumns, enterRows, enterMinValueGenerated, enterMaxValueGenerated, enterNumbersAfterDecimalPoint));
+
+// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void ArithmeticMeanArrayColumnElements(int columns, int rows, int start, int end)
+{
+    int[,] arr = new int[columns, rows];
+
+    double value = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(start, end);
+            Console.Write("{0,8} ", arr[i, j]);           
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            value = value + arr[i,j];
+        }
+        value = value / columns;
+        Console.Write("{0,08} " , Math.Round(value, 1));
+        value = 0;     
+    }
+}
+ArithmeticMeanArrayColumnElements(enterColumns, enterRows, enterMinValueGenerated, enterMaxValueGenerated);
